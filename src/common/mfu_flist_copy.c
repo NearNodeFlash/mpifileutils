1b4f67419bee56b7a8b6ed1be76ba80c1dbac03a
Blake Devcich <blake.devcich@hpe.com>
2024-09-18 11:50:50 -0500

    Fix

--- src/common/mfu_flist_copy.c
+++ src/common/mfu_flist_copy.c
@@ -2176,7 +2176,7 @@ static int mfu_copy_files(
 
         // Cause a segfault
         MFU_LOG(MFU_LOG_INFO, "BLAKE: should I cause a segfault?");
-        if copy_opts->crash && i > 5 {
+        if (copy_opts->crash && i > 5) {
             MFU_LOG(MFU_LOG_INFO, "BLAKE: I should!");
             *((char *) NULL) = 0;
         }
