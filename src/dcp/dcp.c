c6cf82005a4dd523a61fed5d6702f964fbd38750
Blake Devcich <blake.devcich@hpe.com>
2024-09-18 11:42:44 -0500

    break the build

--- src/dcp/dcp.c
+++ src/dcp/dcp.c
@@ -100,6 +100,7 @@ void print_usage(void)
     printf("  -v, --verbose            - verbose output\n");
     printf("  -q, --quiet              - quiet output\n");
     printf("  -h, --help               - print usage\n");
+    printf("  -Z, --cras               - cause a segfault\n");
     printf("For more information see https://mpifileutils.readthedocs.io.\n");
     printf("\n");
     fflush(stdout);
@@ -169,6 +170,7 @@ int main(int argc, char** argv)
         {"verbose"              , no_argument      , 0, 'v'},
         {"quiet"                , no_argument      , 0, 'q'},
         {"help"                 , no_argument      , 0, 'h'},
+        {"crash"                , no_argument      , 0, 'Z'},
         {0                      , 0                , 0, 0  }
     };
 
@@ -345,6 +347,9 @@ int main(int argc, char** argv)
                 /* since process won't be printed in quiet anyway,
                  * disable the algorithm to save some overhead */
                 mfu_progress_timeout = 0;
+                break;
+            case 'Z':
+                mfu_copy_opts->crash = true;
                 break;
             case 'h':
                 usage = 1;
